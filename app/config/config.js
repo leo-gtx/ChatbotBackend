1a9c2bc276dec9896319171b51808cba58e1966f
leonel <dongmoyoyo@gmail.com>
2020-08-05 05:45:46 +0100

    "Last commit"

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 var config = {
     VERSION: 1,
     BUILD: 1,
-    URL: 'http://127.0.0.1' || process.env.URL,
+    URL: 'http://127.0.0.1',
     API_PATH: '/api',
     PORT: process.env.PORT || 8080,
     DB: {
@@ -14,7 +14,7 @@ var config = {
      * Get DB Connection String for connecting to MongoDB database
      */
     getDBString: function() {
-        return 'mongodb://' + this.DB.HOST + ':' + this.DB.PORT + '/' + this.DB.DATABASE;
+        return process.env.MONGODB_URI || 'mongodb://' + this.DB.HOST + ':' + this.DB.PORT + '/' + this.DB.DATABASE;
     },
     /*
      * Get the http URLGoalKicker.com – Node.js Notes for Professionals 184
